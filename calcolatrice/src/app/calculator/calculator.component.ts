da10243e3466a1b53b3a4b0537be7dbe03433335
IzzoDev <aizzo450@gmail.com>
2024-07-23 17:06:46 +0200

    v1.0

--- calcolatrice/src/app/calculator/calculator.component.ts
+++ calcolatrice/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -10,21 +10,38 @@ export class CalculatorComponent implements OnInit{
   secondOperand : number | null = null
   operator : string = ''
   result : number | null = null;
-  display : string | undefined = ''
+  display : string | undefined = '0'
+  swipeDirection : 'right' | 'left' = 'left'
+  swipeClass: string = ''
+  backgroundColorClass : string = 'background-color-1'
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
-
+    this.renderer.addClass(document.body, 'background-color-transition');
   }
 
   clearCalculator(){
-    this.display = ''
+    this.display = '0'
     this.firstOperand = null
     this.secondOperand = null
     this.operator = ''
   }
 
+  clearOnlyOne(){
+    if (this.display!.length > 1) {
+      this.display = this.display?.slice(0,-1)
+    } else {
+      this.display = '0'
+    }
+  }
+
   appendNumber(number:string){
-    this.display += number;
+    if (this.display === '0') {
+      this.display = number; 
+    }else{
+      this.display += number;
+    }
   }
 
   setOperatore(op:string){
@@ -52,11 +69,26 @@ export class CalculatorComponent implements OnInit{
           this.result = this.firstOperand * this.secondOperand
           break;
       }
-      this.display = this.result?.toString()
+      this.display = this.result?.toString() || '0'
       this.firstOperand = null
       this.secondOperand = null
       this.operator = ''
     }
   }
   
+
+  swipe(){
+    // this.swipeClass = this.swipeDirectionn === 'left' ? 'swipe-right' : 'swipe-left'
+
+    // this.swipeDirectionn = this.swipeDirectionn === 'rigth' ? 'left' : 'rigth'
+    if (this.swipeDirection === 'right') {
+      this.swipeClass = 'swipe-left';
+      this.backgroundColorClass = 'background-color-1'
+      this.swipeDirection = 'left'; // Cambia direzione per il prossimo clic
+    } else {
+      this.swipeClass = 'swipe-right';
+      this.backgroundColorClass = 'background-color-2'
+      this.swipeDirection = 'right'; // Cambia direzione per il prossimo clic
+    }
+  }
 }
